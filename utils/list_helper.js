064c547d88744219b549bab8f9a5d735882eb295
jacob l <jacobinmt47@outlook.com>
2019-10-13 16:53:58 -0600

    add favorite blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,24 @@
 const dummy = (blogs) => 1
 const gummy = () => 2
-
-module.exports = { dummy, gummy }
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item.likes
+  }
+  return blogs.length === 0
+    ? 0
+    : blogs.reduce(reducer, 0)
+}
+const favoriteBlog = (blogs) => {
+  let i = 0
+  let likeMax = 0
+  let indexMax = 0
+  for (;i < blogs.length; i += 1) {
+    if (blogs[i].likes > likeMax) {
+      likeMax = blogs[i].likes
+      indexMax = i
+    }
+  }
+  return blogs[indexMax]
+}
+module.exports = { dummy, gummy, totalLikes, favoriteBlog,
+}
