42292cd437f9f8abf790e96d6a1ba0b038673be4
jacob l <jacobinmt47@outlook.com>
2019-10-20 12:24:35 -0600

    fails

--- test/blog_api.test.js
+++ test/blog_api.test.js
@@ -1,2 +1,26 @@
+const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const Blog = require('../models/blog')
+const blogHelper = require('./blog_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  Blog.deleteMany({})
+  // eslint-disable-next-line no-restricted-syntax
+  for (const b of blogHelper) {
+    const blogObject = new Blog(b)
+    blogObject.save()
+  }
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
+
+test('number of blogs', async () => {
+  const response = await api.get('/api/blogs')
+  console.log(response.body.length)
+  expect(response.body.length === 5)
+})
