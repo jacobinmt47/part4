6b73bfb7ffba0a90afe6182b2a98c98ea4c15580
jacob l <jacobinmt47@outlook.com>
2019-11-01 15:54:08 -0600

    added post method

--- controller/user.js
+++ controller/user.js
@@ -7,4 +7,20 @@ UserRouter.get('/', async (request, response) => {
   response.json(users.map(u => u.toJSON()))
 })
 
+UserRouter.post('/', async (request, response, next) => {
+  try {
+    const { body } = request
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+    const user = User({
+      userName: body.userName,
+      name: body.name,
+      passwordHash,
+    })
+    const savedUser = await user.save()
+    response.json(savedUser)
+  } catch (exception) {
+    next(exception)
+  }
+})
 module.exports = UserRouter
